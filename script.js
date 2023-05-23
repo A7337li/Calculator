21abc016d8210dbeea298ed079da11c68dc03eab
A7337li <alizahra7337@gmail.com>
2023-05-23 12:24:34 +0330

    new

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const $ = document;
 const box = $.getElementById('inp');
-let numb = [];
 
 const Entrance = (x) => box.value += x;
 
 const clean = (y) => {
     box.value = y;
-    numb.splice(0);
 }
 
 function total() {

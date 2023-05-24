ae3da63d4832e914797a70a6d6a346c6e8564f30
A7337li <alizahra7337@gmail.com>
2023-05-24 16:37:01 +0330

    change to engineering calculator

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 const $ = document;
 const box = $.getElementById('inp');
+const box2 = $.getElementById('inp2');
+let po = false;
+let per = false;
 
-const Entrance = (x) => box.value += x;
+function Entrance(x) {
+    box.value += x;
 
-const clean = (y) => {
-    box.value = y;
+    if (po) {
+        let sp = box.value.split("^");
+        box2.value = Math.pow(sp[0], sp[1]);
+    } else if (per) {
+        let pe = box.value.split("%*");
+        box2.value = pe[0] / 100 * pe[1];
+    }
+
 }
 
-function total() {
-    box.value = eval(box.value);
+function clean(y) {
+    box.value = y;
+    box2.value = y;
+    per = false;
+    po = false;
+}
+
+const total = () => box2.value = eval(box.value);
+
+function mathfa(ma) {
+    if (ma == "Percent") {
+        per = true;
+        box.value += "%*";
+    } else if (ma == 'pow') {
+        po = true;
+        box.value += "^";
+    } else {
+        box2.value = Math[ma](box.value);
+    }
 }

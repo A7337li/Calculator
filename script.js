33a0bcec23267643a05c892aab921bb8a1537673
A7337li <alizahra7337@gmail.com>
2023-06-02 14:04:12 +0330

    fix bug in use with clean length

--- script.js
+++ script.js
@@ -21,12 +21,12 @@ function mathfa(ma) {
 function Entrance(x) {
     box.value += x;
 
-    if (po) {
-        let sp = box.value.split("^");
-        box2.value = Math.pow(sp[0], sp[1]);
-    } else if (per) {
+    if (per) {
         let pe = box.value.split("%*");
         box2.value = pe[0] / 100 * pe[1];
+    } else if (po) {
+        let sp = box.value.split("^");
+        box2.value = Math.pow(sp[0], sp[1]);
     } else if (eva) {
         box2.value = eval(box.value);
     }
@@ -44,6 +44,16 @@ function clean() {
 function cleanle() {
     let len = box.value.length;
     box.value = box.value.substr(0, len - 1);
+    if (box.value.indexOf("^") == -1
+        && box.value.indexOf("+") == -1
+        && box.value.indexOf("-") == -1
+        && box.value.indexOf("*") == -1
+        && box.value.indexOf("/") == -1
+        && box.value.indexOf("%") == -1) {
+        po = false;
+        per = false;
+        eva = false;
+    }
 }
 
 function total() {
@@ -53,3 +63,48 @@ function total() {
     per = false;
     eva = false;
 }
+
+function kyPhysical() {
+    document.addEventListener('keydown', function (event) {
+        switch (event.key) {
+            case 'Backspace':
+                let len = box.value.length;
+                box.value = box.value.substr(0, len - 1);
+                break;
+
+            case '0':
+            case '.':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                box.value += event.key;
+                box2.value = eval(box.value);
+                break;
+
+            case '=':
+                box.value = box2.value;
+                box2.value = "";
+                po = false;
+                per = false;
+                eva = false;
+                break;
+
+            default:
+                box.value += '';
+                break;
+        }
+
+    })
+}
+
+kyPhysical();
